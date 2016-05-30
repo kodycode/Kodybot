e2d2879ca5bebd837653c2c7b91b75116aa4d044
kodycode <kodythach@gmail.com>
2016-05-30 19:44:13 -0400

    Fixed error, updated function names, fixed summoner name displaying

--- lolbot.py
+++ lolbot.py
@@ -5,30 +5,33 @@ import discord
 import logger
 client = discord.Client()
 
-async def getSummonerID(region, summoner_name, API_key):
+async def get_Summoner_ID(region, summoner_name, API_key):
     URL = 'https://' + region + '.api.pvp.net/api/lol/' + region + '/v1.4/summoner/by-name/' + summoner_name + '?api_key=' + API_key
     response = requests.get(URL)
     print(URL)
     return response.json()
 
-async def requestRankedData(region, ID, API_key):
+async def request_Ranked_Data(region, ID, API_key):
 
     URL = 'https://' + region + '.api.pvp.net/api/lol/' + region + '/v2.5/league/by-summoner/' + ID + '/entry?api_key=' + API_key
     response = requests.get(URL)
     print(URL)
     return response.json()
 
-async def main(message, summoner_name):
+async def print_ranked_data(message, summoner_name):
+    summoner = summoner_name.replace(' ', '')
+    summoner = summoner.lower()
+    
     region = 'na'       #Change to any other region if so desired
     
     API_key = 'Enter LoL API Key Here';
     #If you don't have one you can get one at https://developer.riotgames.com
 
-    responseJSON = await summonerInfo(region, summoner_name, API_key)
+    responseJSON = await get_Summoner_ID(region, summoner, API_key)
 
-    ID = responseJSON[summoner_name]['id']
+    ID = responseJSON[summoner]['id']
     ID = str(ID)          
-    responseJSON2 = await requestRankedData(region, ID, API_key)
+    responseJSON2 = await request_Ranked_Data(region, ID, API_key)
 
     tier = responseJSON2[ID][0]['tier']
     division = responseJSON2[ID][0]['entries'][0]['division']
@@ -46,12 +49,12 @@ async def on_message(message):
         client.logout()
         print('Bot has logged out')
     if message.content.startswith('$rank'):
-        summoner_name = message.content[6:].lower()
+        summoner_name = message.content[6:]
         
         try:
-            await main(message, summoner_name)
+            await print_ranked_data(message, summoner_name)
         except KeyError:
             await client.send_message(message.channel, 'ERROR! No ranked stats found for this player')
-
+            
 client.run('Enter discord token/account information here')
 client.connect()
